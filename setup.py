446d63796aea50119625ce5fc10a7a72fed51bdf
Andrew <bg_zero_x12@yahoo.com>
2016-06-07 14:55:20 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,15 +1,15 @@
 from setuptools import setup, find_packages
 
 
-setup(name="freeopcua-client",
-      version="0.4.5",
-      description="Minimal OPC-UA Client GUI",
-      author="Olivier R-D",
-      url='https://github.com/FreeOpcUa/opcua-client-gui',
-      packages=["freeopcuaclient"],
+setup(name="opcua2azure",
+      version="0.1.0",
+      description="OPC-UA to Azure IoT Hub",
+      author="Olivier R-D, Andrew J",
+      url='https://github.com/zeroentity/opcua-2-azure/',
+      packages=["opcua2azure"],
       license="GNU General Public License",
       install_requires=["freeopcua"],
       entry_points={'console_scripts':
-                    ['freeopcua-client = freeopcuaclient.mainwindow:main']
+                    ['opcua2azure = opcua2azure.mainwindow:main']
                     }
       )
